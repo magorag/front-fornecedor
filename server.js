658d1410e39d0b92bf17fc41252bf455d0f3b003
Luccas Piola <luccas.hcpiola@gmail.com>
2019-09-25 17:32:49 -0300

    history

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
+const history = require('connect-history-api-fallback');
 
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
