b25a1cc2d79a0cacdfb7c15954e4ad219d5e31fa
Luccas Piola <luccas.hcpiola@gmail.com>
2019-09-25 17:29:38 -0300

    history

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var serveStatic = require('serve-static')
 
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(history({
+    disableDotRule: true,
+    verbose: true
+}));
 
 var port = process.env.PORT || 5000
 app.listen(port)
